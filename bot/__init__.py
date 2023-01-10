4061aac83a2ea6a6af14b425955ceb10f2f804a8
amiralipoor20072007 <amiralipoor20072007@gmail.com>
2023-01-11 00:22:02 +0330

    delete qbittorrent

--- bot/__init__.py
+++ bot/__init__.py
@@ -2,7 +2,6 @@ from logging import getLogger, FileHandler, StreamHandler, INFO, basicConfig, er
 from socket import setdefaulttimeout
 from faulthandler import enable as faulthandler_enable
 from telegram.ext import Updater as tgUpdater, Defaults
-from qbittorrentapi import Client as qbClient
 from aria2p import API as ariaAPI, Client as ariaClient
 from os import remove as osremove, path as ospath, environ
 from subprocess import Popen, run as srun
