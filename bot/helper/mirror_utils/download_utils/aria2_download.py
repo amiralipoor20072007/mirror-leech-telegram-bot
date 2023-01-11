4abac83208d278e8bce093f4f6db52e4b5935b33
amiralipoor20072007 <amiralipoor20072007@gmail.com>
2023-01-12 02:31:14 +0330

    Fixed Removing GoogleDrive

--- bot/helper/mirror_utils/download_utils/aria2_download.py
+++ bot/helper/mirror_utils/download_utils/aria2_download.py
@@ -3,7 +3,7 @@ from os import remove, path as ospath
 import requests as req
 
 from bot import aria2, download_dict_lock, download_dict, LOGGER, config_dict
-from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
+# from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
 from bot.helper.ext_utils.bot_utils import is_magnet, getDownloadByGid, new_thread, bt_selection_buttons
 from bot.helper.mirror_utils.status_utils.aria_download_status import AriaDownloadStatus
 from bot.helper.telegram_helper.message_utils import sendMessage, deleteMessage, update_all_messages
@@ -50,12 +50,12 @@ def __onDownloadStarted(api, gid):
                         sname = get_base_name(sname)
                     except:
                         sname = None
-                if sname is not None:
-                    smsg, button = GoogleDriveHelper().drive_list(sname, True)
-                    if smsg:
-                        listener.onDownloadError('File/Folder already available in Drive.\n\n')
-                        api.remove([download], force=True, files=True)
-                        return sendMessage("Here are the search results:", listener.bot, listener.message, button)
+                # if sname is not None:
+                #     smsg, button = GoogleDriveHelper().drive_list(sname, True)
+                #     if smsg:
+                #         listener.onDownloadError('File/Folder already available in Drive.\n\n')
+                #         api.remove([download], force=True, files=True)
+                #         return sendMessage("Here are the search results:", listener.bot, listener.message, button)
     except Exception as e:
         LOGGER.error(f"{e} onDownloadStart: {gid} check duplicate didn't pass")
 
